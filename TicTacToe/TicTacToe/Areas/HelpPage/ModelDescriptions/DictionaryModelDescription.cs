838f31beb111c9d7b8d34bd4c0147ae8c39acb4c
Bharath Reddy Aemireddy <bhre@microsoft.com>
2017-07-10 18:40:39 -0700

    First commit

---
+++ TicTacToe/TicTacToe/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TicTacToe.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TicTacToe/TicTacToe/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace TicTacToe.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
